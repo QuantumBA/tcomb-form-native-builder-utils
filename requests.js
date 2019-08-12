a947a835e10c437d03bc15076cdb320c130710f8
Sergio <smartinez@foqum.io>
2019-08-12 15:53:20 +0200

    Added the possibility to define the fields that will be updated for sublists

--- requests.js
+++ requests.js
@@ -2,7 +2,6 @@ import { createApolloFetch } from 'apollo-fetch'
 import stringTemplate from '@foqum/string-template'
 import { getPayloadError } from 'getpayload'
 import objectToFormData from '@foqum/object-to-formdata'
-
 // Private helpers functions
 const removeEmptyKeys = (obj) => {
   if (!obj) return
@@ -13,14 +12,11 @@ const removeEmptyKeys = (obj) => {
     }
   })
 }
-
 const isString = x => typeof x === 'string'
-
 const getFieldName = (key) => {
   if (isString(key)) return key
   return Object.values(key)[0]
 }
-
 // Object serialization without quotes around the keys.
 // To be used with graphQL
 function stringify(input, depth = 0) {
@@ -29,10 +25,8 @@ function stringify(input, depth = 0) {
     const obj = Object.values(input).map(v => `{${stringify(v)}}`)
     return `[${obj}]`
   }
-
   // native type
   if (typeof input !== 'object') return JSON.stringify(input)
-
   // object
   depth += 1
   let stringArray = Object.keys(input).map(key => `${key}:${stringify(input[key], depth)}`)
@@ -42,6 +36,11 @@ function stringify(input, depth = 0) {
   return stringArray.join(',')
 }
 
+function getArrayOfsubItems(array, startCharacter, endCharacter) {
+  const start = array.indexOf(startCharacter)
+  const end = array.indexOf(endCharacter)
+  return array.slice(start + 1, end).split(',').map(field => field.trim())
+}
 async function processRemoteRequests(uriTemplate, stackedPlaceholders, headers, body = undefined) {
   let response = {}
   const input = stringTemplate(uriTemplate, stackedPlaceholders, /\$\{([0-9a-zA-Z_\.]+)\}/g, '${') // eslint-disable-line
@@ -53,20 +52,16 @@ async function processRemoteRequests(uriTemplate, stackedPlaceholders, headers, 
     const apolloFetch = createApolloFetch({
       uri: uriTemplate,
     })
-
     response = await apolloFetch({ query: body })
   }
   return response
 }
-
 async function processRemoteUpdateGraphQL(uri, body, meta) {
   body = JSON.stringify(body, (key, value) => (value === null ? '' : value))
   const body_json = JSON.parse(body)
   removeEmptyKeys(body_json)
-
   const body_query = stringify(body_json)
   const apolloFetch = createApolloFetch({ uri })
-
   apolloFetch.useAfter(({ response }, next) => {
     if (response.parsed.errors) {
       throw response.parsed.errors[0]
@@ -76,47 +71,36 @@ async function processRemoteUpdateGraphQL(uri, body, meta) {
   if (meta.graphql.method) {
     const response_fields = meta.graphql.response_fields ? meta.graphql.response_fields.join(' ') : '_id'
     const mutation = `mutation { response: ${meta.graphql.method}(${body_query}) {${response_fields}} }`
-
     return apolloFetch({ query: mutation })
   }
-
   // method not in method (retrocompatibility)
   const mutation = `mutation { ${meta.graphql}(${body_query}) {response message} }`
-
   return apolloFetch({ query: mutation }).then(resp => resp)
-
 }
-
 async function processRemoteUpdateRest(uri, body, contentType, headers) {
-
   if (contentType === 'application/json') {
     body = JSON.stringify(body, (key, value) => (value === null ? '' : value))
   } else {
     body = objectToFormData(body)
   }
-
   return fetch(uri, {
     method: 'POST',
     headers,
     body,
   }).then(getPayloadError).then(resp => resp)
-
 }
-
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index += 1) {
     await callback(array[index], index, array) // eslint-disable-line
   }
 }
-
 async function processListRemoteUpdate(remote, formValues) {
-
   const responses = []
   const responsesFn = async () => {
     await asyncForEach(remote, async (remoteObject) => {
       const { meta, uri } = remoteObject
       const { headers = {}, contentType = 'multipart/form-data', updateFields } = meta
-      let postBody = JSON.parse(JSON.stringify(formValues))
+      const postBody = JSON.parse(JSON.stringify(formValues))
       Object.keys(postBody).forEach((k) => {
         if (Array.isArray(postBody[k])) {
           postBody[k].forEach((v) => {
@@ -131,10 +115,28 @@ async function processListRemoteUpdate(remote, formValues) {
       })
       // set fields sent to graphql resolver accessing form values and/or previous response
       // if not informed it will select all values from the form
+      let bodyPost = {}
       if (updateFields) {
-        postBody = Object.keys(postBody)
-          .filter(field => updateFields.includes(field))
-          .reduce((obj, field) => {return { ...obj, [field]: postBody[field] } }, {}) // eslint-disable-line
+        bodyPost = {}
+        Object.entries(postBody).forEach(([field, fieldValue]) => {
+          updateFields.map((updateField) =>  {
+            if (updateFields.includes(field)) {
+              bodyPost[field] = fieldValue
+              // If updated fields are in a sublist
+            } else if (isString(updateField) && updateField.indexOf('{') !== -1 && updateField.startsWith(field)) {
+              const arrayOfItems = []
+              // Retrieve field names from 'updateFields' string
+              const updateFieldsSplitted = getArrayOfsubItems(updateField, '{', '}')
+              fieldValue.forEach((items) => {
+                const requiredValuesForObject = Object.keys(items)
+                  .filter(field => updateFieldsSplitted.includes(field))
+                  .reduce((obj, item) => {return { ...obj, [item]: items[item] } }, {}) // eslint-disable-line
+                arrayOfItems.push(requiredValuesForObject)
+              })
+              bodyPost[field] = arrayOfItems
+            }
+          })
+        })
 
         const previousFields = updateFields.filter(field => getFieldName(field).includes('_prev') && isString(field))
         if (previousFields.length > 0) {
@@ -142,10 +144,9 @@ async function processListRemoteUpdate(remote, formValues) {
             const splitArray = getFieldName(previusField).split('.')
             const nameField = splitArray[splitArray.length - 1]
             const indexResponse = parseInt(getFieldName(previusField).split('[')[1].split(']')[0], 10)
-            postBody[nameField] = responses[indexResponse].data.response[nameField]
+            bodyPost[nameField] = responses[indexResponse].data.response[nameField]
           })
         }
-
         // encapsulate fields in objects if necessary
         const objectsUpdateFields = updateFields.filter(key => !isString(key))
         objectsUpdateFields.forEach((objectField) => {
@@ -153,28 +154,23 @@ async function processListRemoteUpdate(remote, formValues) {
           if (fieldName.startsWith('_prev[')) {
             const realFieldName = fieldName.split('.')[1]
             const indexResponse = parseInt(fieldName.split('[')[1].split(']')[0], 10)
-            postBody[Object.keys(objectField)[0]] = Object.assign({}, { [realFieldName]: responses[indexResponse].data.response[realFieldName] })
+            bodyPost[Object.keys(objectField)[0]] = Object.assign({}, { [realFieldName]: responses[indexResponse].data.response[realFieldName] })
           } else {
-            postBody[Object.keys(objectField)[0]] = Object.assign({}, { [fieldName]: formValues[fieldName] })
+            bodyPost[Object.keys(objectField)[0]] = Object.assign({}, { [fieldName]: formValues[fieldName] })
           }
         })
       }
-
       // Graphql API
       if (meta.graphql) {
-        responses.push(await processRemoteUpdateGraphQL(uri, postBody, meta))
+        responses.push(await processRemoteUpdateGraphQL(uri, bodyPost, meta))
       } else {
-        responses.push(await processRemoteUpdateRest(uri, postBody, contentType, headers, meta))
+        responses.push(await processRemoteUpdateRest(uri, bodyPost, contentType, headers, meta))
       }
-
     })
-
     return responses
   }
   return Promise.all(await responsesFn())
-
 }
-
 export {
   processRemoteRequests,
   processRemoteUpdateGraphQL,
